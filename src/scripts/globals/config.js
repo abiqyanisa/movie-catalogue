e09e5fc9b4a0d2b6227538cc8734f1eb20f86805
Fathimah Abiyyi Khairunnisa <93188912+abiqyanisa@users.noreply.github.com>
2024-06-18 09:37:45 +0700

    Add files via Upload

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+  KEY: '91b56c1b11a0cfd64b118c220166fa89',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MovieCatalogue-V1',
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
+};
+
+export default CONFIG;
